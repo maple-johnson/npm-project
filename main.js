62d4c737f65b0163d74d7494956a7ea593b3aea5
Maple <135763163+maple-johnson@users.noreply.github.com>
2025-04-04 19:34:09 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+import lodash from 'lodash';
+import chalk from 'chalk';
+
+const array = [1, 4, 2, 3, 1, 4, 5, 2, 3, 2, 1];
+const uniqueNums = lodash.uniq(array);
+console.log(chalk.yellow(uniqueNums));
+
+console.log(chalk.blueBright("Hello, with chalk!"));
